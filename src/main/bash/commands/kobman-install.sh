2dd2ec1ff3a078c485fd1209e590a96aa99cc8b2
Arun Suresh <arunsureshampadath@gmail.com>
2020-01-26 00:57:58 +0530

    password visibility

--- src/main/bash/commands/kobman-install.sh
+++ src/main/bash/commands/kobman-install.sh
@@ -414,7 +414,7 @@ function __kobman_check_proxy {
          	sudo echo -e "\n"
          	read -p "Enter AD ID? [eg :ai318974]                 :" uname
          	sudo echo -e "\n"
-         	read  -p "Enter password?[your login password]        : " pword
+         	read -s -p  "Enter password?[your login password]        : " pword
          	sudo echo -e "\n"
          	read -p "Enter email ID?                             :" emil
          	sudo echo -e "\n"
