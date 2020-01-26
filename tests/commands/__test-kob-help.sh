685b253aa3b75cd21c1f4210f83720a6c79e76b3
Arun Suresh <arunsureshampadath@gmail.com>
2020-01-26 17:43:13 +0530

    test script updated

--- tests/commands/__test-kob-help.sh
+++ tests/commands/__test-kob-help.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
-
-sudo figlet Testing
-sudo figlet help
-sudo figlet command
-
-
-
-
+if [ -z "$KOBMAN_DIR" ]; then
+	export KOBMAN_DIR="~/.kobman/"
+fi
 #ls -a > list.txt
+cd ~/
 if ls -a | grep .kobman
-then
-sudo figlet REMOVING 
-sudo figlet KOBMAN 
-sudo rm -rv .kobman
-sudo figlet INSTALLING
-sudo figlet KOBMAN 
-curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash
-source "/home/arun/.kobman/bin/kobman-init.sh"
+	then
+		sudo figlet REMOVING 
+		sudo figlet KOBMAN 
+		sudo rm -rv .kobman
+		sudo figlet INSTALLING
+		sudo figlet KOBMAN 
+		curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash
+		#sudo rm -rv .kobman/src/kobman-help.sh
+		source "/${KOBMAN_DIR}/bin/kobman-init.sh"
+	else
+		curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash
+		#sudo rm -rv .kobman/src/kobman-help.sh
+		source "/${KOBMAN_DIR}/bin/kobman-init.sh"
 fi
 #kob help > help.txt
 
-if kob help | grep -E -- 'install|list|help|version'
-then
+if kob help | grep -x "Usage: kob <command> [environment] 
+
+   commands:
+       install   or i    [--dev] <environment> [namespace]
+       uninstall or rm   <environment> <version>
+       list      or ls   [environment]
+       version   or v
+       status or s
+       help      or h
+
+   environment  :  the KOB to install: tob,tobvon,greenlight.. etc.
+                 use list command for comprehensive list of environments
+                 eg: $ kob list
+   --dev        :for setting up development environment
+   --deploy     :for deploying specific environment
+                 eg: $ kob --dev install tobvon
+   namespace    :  you can use this for setup dev env from you forked version
+                 eg: $ kob install --dev tobvon hyperledgerkochi
+"
+	then
  
-	sudo figlet SUCCESS 
+		sudo figlet SUCCESS 
 
 
-else
-	sudo figlet FAIL 
+	else
+		sudo figlet FAIL 
 
 fi
 #sudo rm help.txt
