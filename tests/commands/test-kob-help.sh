80e46445a5123ee387633130626038052922f75c
Arun Suresh <arunsureshampadath@gmail.com>
2020-02-02 00:49:07 +0530

    changes made

--- tests/commands/test-kob-help.sh
+++ tests/commands/test-kob-help.sh
@@ -16,41 +16,23 @@ if ls -a | grep .kobman
 	then
 		sudo figlet REMOVING 
 		sudo figlet KOBMAN 
-		sudo rm -rv .kobman
+		sudo rm -rv .kobman > output.txt
 		sudo figlet INSTALLING
 		sudo figlet KOBMAN 
-		curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash
+		curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash > output.txt
 		#sudo rm -rv .kobman/src/kobman-help.sh
 		source "/$KOBMAN_DIR/bin/kobman-init.sh"
 
 #if .kobman not found	
 	else
-		curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash
+		curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash > output.txt
 		#sudo rm -rv .kobman/src/kobman-help.sh
 		source "/$KOBMAN_DIR/bin/kobman-init.sh"
 fi
 #kob help > help.txt
 
 #testing help command for pattern matching
-if kob help | grep -x "Usage: kob <command> [environment] 
-
-   commands:
-       install   or i    [--dev] <environment> [namespace]
-       uninstall or rm   <environment> <version>
-       list      or ls   [environment]
-       version   or v
-       status or s
-       help      or h
-
-   environment  :  the KOB to install: tob,tobvon,greenlight.. etc.
-                 use list command for comprehensive list of environments
-                 eg: $ kob list
-   --dev        :for setting up development environment
-   --deploy     :for deploying specific environment
-                 eg: $ kob --dev install tobvon
-   namespace    :  you can use this for setup dev env from you forked version
-                 eg: $ kob install --dev tobvon hyperledgerkochi
-"
+if ((kob help | grep "install") && (kob help | grep "uninstall") && (kob help | grep "help") && (kob help | grep "status") && (kob help | grep "list")) > output.txt
 	then
 #if pattern matched 
 		sudo figlet SUCCESS 
