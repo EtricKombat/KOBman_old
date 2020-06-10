fbc0708b7b0269db50adab29ba02d8442e095d25
EtricKombat <ericjoy1991@gmail.com>
2020-06-10 13:05:03 +0530

    dopublish updated

--- bin/dopublish.sh
+++ bin/dopublish.sh
@@ -19,17 +19,17 @@ git checkout $branch
 mkdir -p build/tmp
 
 # making of zip files
-zip -r build/tmp/kobman-latest.zip ~/KOBman/src/
+zip -r build/tmp/kobman-latest.zip ~/Documents/KOBman/src/
 cp build/tmp/kobman-latest.zip build/tmp/kobman-$kob_rel_version.zip
 
 # moving get.kobman.io to tmp/
-mv ~/KOBman/scripts/get.kobman.io build/tmp/
+mv ~/Documents/KOBman/scripts/get.kobman.io build/tmp/
 
 # moving into dist branch
 git checkout $dist_branch
 
 # collecting files from Release branch tmp/ folder to dist branch
-git checkout $branch -- ~/KOBman/build/tmp/* &> /dev/null
+git checkout $branch -- ~/Documents/KOBman/build/tmp/* &> /dev/null
 
 mkdir dist &> /dev/null
 # moving of latest files from tmp/ to dist/
@@ -41,7 +41,7 @@ git commit -m "Released the version $kob_rel_version"
 git push origin -f $dist_branch
 
 #checkout back to master
-git checkout master
+git checkout Demo_hyperledgerkochi 
 
 
 
