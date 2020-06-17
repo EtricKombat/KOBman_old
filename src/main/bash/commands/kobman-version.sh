bc14a3d30df1cd05501623d9fde9a4b58bb3c901
EtricKombat <ericjoy1991@gmail.com>
2020-06-17 20:45:40 +0530

    testing version for environment variables

--- src/main/bash/commands/kobman-version.sh
+++ src/main/bash/commands/kobman-version.sh
@@ -3,18 +3,21 @@
 
 function __kob_version {
 
- 	local command deployment_type environment_type name_space 
+ 	local command deployment_type environment_type name_space
 	command=$COMMAND
 	deployment_type=$DEPLOYMENT_TYPE
 	environment_type=$ENVIRONMENT_TYPE
-	name_space=$NAME_SPACE                                                                   
-	if [ "$DEPLOYMENT_TYPE" == "" ] 
+	name_space=$NAME_SPACE
+  echo "deployment_type=" "$deployment_type"
+  echo "environment_type=" "$environment_type"
+  echo "name_space=" "$name_space"
+  if [ "$DEPLOYMENT_TYPE" == "" ]
 	then
-		cd "${KOBMAN_DIR}/var"	
-		cat version.txt	
-		cd ~	
-	else	
-		__kobman_environment_version  
+		cd "${KOBMAN_DIR}/var"
+		cat version.txt
+		cd ~
+	else
+		__kobman_environment_version
 	fi
 }
 
@@ -22,52 +25,52 @@ function __kob_version {
 
 function __kobman_environment_version {
 
- 	local name_space 
+ 	local name_space
 	name_space=$1
-	if [ "$DEPLOYMENT_TYPE" == "--environment" ] 
-	then	
-	
+	if [ "$DEPLOYMENT_TYPE" == "--environment" ]
+	then
+
 		case "$ENVIRONMENT_TYPE" in
 		tobvon)
-			__kobman_version_tobvon 
+			__kobman_version_tobvon
 
-		;;	
+		;;
 		tob)
-			__kobman_version_tob 
+			__kobman_version_tob
 
-		;;	
+		;;
 		greenlight)
-			__kobman_version_greenlight 
-		;;	
+			__kobman_version_greenlight
+		;;
 		kobman)
-			__kobman_version_kobman 
+			__kobman_version_kobman
 
-		;;	
+		;;
 		kobvon)
 			echo "invoking kobman version"
-			__kobman_version_kobvon 
+			__kobman_version_kobvon
 
-		;;	
+		;;
 		kob)
-			__kobman_version_kob 
+			__kobman_version_kob
 
-		;;	
+		;;
 		kobdflow)
-			__kobman_version_kobdflow 
+			__kobman_version_kobdflow
 
-		;;	
+		;;
 		kobconnect)
-			__kobman_version_kobconnect 
+			__kobman_version_kobconnect
 
-		;;	
+		;;
 		kobregistery)
 			__kobman_version_kobregistery
 
 		;;
 		"")
-			echo "verify your command &  try again"	
+			echo "verify your command &  try again"
 
-		;;	
+		;;
 	esac
 	fi
 }
