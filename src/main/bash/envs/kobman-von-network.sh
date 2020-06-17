1a5d207d96c31144aaa0b1092c8de4606de7605a
EtricKombat <ericjoy1991@gmail.com>
2020-06-17 14:16:12 +0530

    cleaning von-network file

--- src/main/bash/envs/kobman-von-network.sh
+++ src/main/bash/envs/kobman-von-network.sh
@@ -1,43 +1,14 @@
 #!/bin/bash
 
-#function __kobman_development_tobvon_dir 
-#{
-#	local namespace="$1"	
-#	local version_id="$2"	
-#	cd ~
-#	if [ ! -d "${KOBMAN_VON_DEV_DIR}/von-network" ]; then 
-#		
-#		__kobman_install_dependancies
-#		mkdir -p Dev_TOBVON
-#		cd Dev_TOBVON
-#		export KOBMAN_VON_DEV_DIR=$PWD
-#		cd ${KOBMAN_VON_DEV_DIR}
-#		mkdir -p test/ dependency/
-#               	__kobman_echo_cyan "Copying source code  of https://github.com/${namespace}/von-network to your local system ! "
-#	else
-#		__kobman_install_dependancies
-#		rm -rf  Dev_TOBVON
-#		mkdir -p Dev_TOBVON
-#		cd Dev_TOBVON
-#		export KOBMAN_VON_DEV_DIR=$PWD
-#		cd ${KOBMAN_VON_DEV_DIR}
-#		mkdir -p test/ dependency/
-#               	__kobman_echo_cyan "Removing existing older version & Copying newer Version : "${version_id}" ,  source code from https://github.com/${namespace}/KOBman into your local system ! "
-#	
-#	fi
-#
-#
-#
-#}                                                                                                                          
 function __kobman_install_von-network
 {
 
 	local environment_name="$1"
-	local version_id="$2"	
+	local version_id="$2"
 
-	cd ~	
-	
-	if [ ! -d "Dev_von-network" ]; then 
+	cd ~
+
+	if [ ! -d "Dev_von-network" ]; then
  		__kobman_echo_white "Creating Dev environment for ${environment_name}"
  		__kobman_echo_white "from https://github.com/${KOBMAN_NAMESPACE}/${environment_name}"
  		__kobman_echo_white "version :${version_id} "
@@ -47,30 +18,30 @@ function __kobman_install_von-network
 #		export Dev_"${environment_name}" = $PWD
 
 		mkdir -p test/ dependency/
-		git clone https://github.com/${KOBMAN_NAMESPACE}/${environment_name} 2> /dev/null	
+		git clone https://github.com/${KOBMAN_NAMESPACE}/${environment_name} 2> /dev/null
 		__kobman_install_dependancies
         	# sudo ~/Dev_von-network/von-network/manage rm
         	sudo ~/Dev_von-network/von-network/manage build
 		 __kobman_echo_violet "Dev environment for ${environment_name} created successfully"
 	else
  		__kobman_echo_white "Removing existing version "
-		rm -rf ~/Dev_"${environment_name}"	
+		rm -rf ~/Dev_"${environment_name}"
  		__kobman_echo_white "Creating Dev environment for ${environment_name}"
  		__kobman_echo_white "from https://github.com/${KOBMAN_NAMESPACE}/${environment_name}"
  		__kobman_echo_white "version :${version_id} "
-		
+
 		cd ~
 		mkdir -p Dev_"${environment}"
 		cd Dev_"${environment}"
 		export Dev_von-network=$PWD
 		mkdir -p test/ dependency/
 
-		git clone https://github.com/${KOBMAN_NAMESPACE}/${environment_name} 2> /dev/null	
+		git clone https://github.com/${KOBMAN_NAMESPACE}/${environment_name} 2> /dev/null
 		__kobman_install_dependancies
         	sudo ~/Dev_von-network/von-network/manage rm
         	sudo ~/Dev_von-network/von-network/manage build
 		__kobman_echo_violet "Dev environment for ${environment_name} created successfully"
-	fi		
+	fi
 	cd ~
 
 
@@ -88,34 +59,19 @@ function __kobman_install_von-network
 function __kobman_start_tobvon
 {
 
- 	__kobman_echo_white "Deploying Verifiable Organizations Network environment from Github namespace : "	
+ 	__kobman_echo_white "Deploying Verifiable Organizations Network environment from Github namespace : "
 	__kobman_echo_green "${kobman_namespace}"
-        cd ${KOBMAN_VON_DEV_DIR} 
+        cd ${KOBMAN_VON_DEV_DIR}
 	von-network/manage start
 }
 
 
 
 
-#################newly incorporated code from kobma-dependancy.sh################
 
 
 
-#function __kobman_create_dev_environment 
-#{
-#	local environment="$1"	
-#	local version_id="$2"	
-#	cd ~
-#	mkdir -p Dev_"${environment}"
-#	cd Dev_"${environment}"
-#	export "${environment}"_DEV_DIR=$PWD
-#	mkdir -p test/ dependency/
-#}
-	
-
-
-
-function __kobman_install_dependancies {     
+function __kobman_install_dependancies {
 
 #	__kobman_check_proxy
   #      __kobman_ubuntu_update_upgrade
@@ -135,7 +91,7 @@ function __kobman_ubuntu_update_upgrade {
 }
 function __kobman_proxy_environment {
 	__kobman_echo_yellow "Unsetting Proxy"
-	unset http_proxy
+				unset http_proxy
         unset ftp_proxy
         unset https_proxy
         unset socks_proxy
@@ -160,9 +116,9 @@ function __kobman_proxy_environment {
 function __kobman_check_proxy {
 
 	__kobman_echo_red "================================="
-	read -p "Behind Proxy ?! [y/n]" reply 
+	read -p "Behind Proxy ?! [y/n]" reply
 	__kobman_echo_red "================================="
-        if [ "$reply" = "y" ] || [ "$reply" = "Y" ] || [ "$reply" = "yes" ] || [ "$reply" = "YES" ] 
+        if [ "$reply" = "y" ] || [ "$reply" = "Y" ] || [ "$reply" = "yes" ] || [ "$reply" = "YES" ]
 	then
 		proxychk=1
 	 	dpkg --configure -a
@@ -202,7 +158,7 @@ function __kobman_check_proxy {
         git config --global http.sslVerify false
         git config --global http.proxy http://${uname}:${pword}@${prox}:${port}
 
-else 
+else
 	echo "Skipping the proxy settings"
 fi
 
@@ -219,7 +175,7 @@ function __kobman_git_install {
 }
 
 function __kobman_python_install {
-	
+
 	__kobman_echo_yellow "Installing Python"
 	sudo apt install software-properties-common -y
         sudo apt install Python -y
@@ -233,8 +189,8 @@ function __kobman_docker_install {
 
         sudo apt-get remove docker docker-engine docker-ce docker-ce-cli docker.io -y
 	curl -fsSL https://get.docker.com -o get-docker.sh
-    	sudo sh get-docker.sh 
-	sudo usermod -aG docker $USER 
+    	sudo sh get-docker.sh
+	sudo usermod -aG docker $USER
 
 #        sudo apt-get remove docker docker-engine docker-ce docker-ce-cli docker.io -y
 #	sudo apt-get update -y
@@ -281,7 +237,7 @@ function __kobman_docker_install {
 }
 
 function __kobman_npm_install {
-	
+
 	__kobman_echo_yellow "Purging existing NPM components"
 	npm config rm proxy
         npm config rm proxy --global
@@ -293,7 +249,7 @@ function __kobman_npm_install {
         npm cache clean
 
         sudo apt-get remove nodejs nodejs-dev node-gyp libssl1.0-dev npm -y
- 
+
 	__kobman_echo_yellow "Installing NPM"
         sudo apt-get install nodejs nodejs-dev node-gyp libssl1.0-dev npm -y
 
@@ -312,7 +268,7 @@ function __kobman_npm_install {
 
 function __kobman_visual_studio_install {
 
-	__kobman_echo_yellow "Installing Visual-Studio"	
+	__kobman_echo_yellow "Installing Visual-Studio"
 	sudo apt install software-properties-common apt-transport-https wget
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
@@ -332,26 +288,26 @@ function __kobman_visual_studio_install {
 
 #function __kobman_uninstall_tobvon
 #{
-# 	__kobman_echo_white "Removing Verifiable Organizations Network environment...  "	
-#	cd ${KOBMAN_VON_DEV_DIR} 
-#	rm -rf von-network/ 2> /dev/null	
+# 	__kobman_echo_white "Removing Verifiable Organizations Network environment...  "
+#	cd ${KOBMAN_VON_DEV_DIR}
+#	rm -rf von-network/ 2> /dev/null
 #	cd ~
-#	rm -rf Dev_TOBVon/ 2> /dev/null	
-#	rm -rf ${KOBMAN_VON_DEV_DIR} 2> /dev/null	
-# 	__kobman_echo_red "Verifiable Organizations Network environment removed !! "	
+#	rm -rf Dev_TOBVon/ 2> /dev/null
+#	rm -rf ${KOBMAN_VON_DEV_DIR} 2> /dev/null
+# 	__kobman_echo_red "Verifiable Organizations Network environment removed !! "
 #	cd ~
 #}
 #
 #function __kobman_version_tobvon
 #{
 #	if [ -d "${KOBMAN_TOBVON_DEV_DIR}" ]
-#	then 
+#	then
 #		kobman_namespace="$1"
-#		cd ${KOBMAN_VON_DEV_DIR} 
-#		cd von-network/	
+#		cd ${KOBMAN_VON_DEV_DIR}
+#		cd von-network/
 #		git show-ref --tag | grep -o v0.0.*
 #		cd ~
 #	else
-# 		__kobman_echo_red " Verifiable Organizations Network environment is not installed in the Local system !"	
+# 		__kobman_echo_red " Verifiable Organizations Network environment is not installed in the Local system !"
 #	fi
 #}
