35e39c0292362d616be27a990cc2c7481d2a8e71
EtricKombat <ericjoy1991@gmail.com>
2020-06-18 15:54:54 +0530

    test-kob-version WIP

--- tests/commands/test-kob-version.sh
+++ tests/commands/test-kob-version.sh
@@ -1,1 +1,109 @@
 
+#!/bin/bash
+
+status="true"
+
+function __test_kob_init
+{
+    echo "Checking for kob..."
+    if [[ -d $KOBMAN_DIR ]]; then
+        source $KOBMAN_DIR/src/kobman-utils.sh
+        __kobman_echo_no_colour "kob found"
+        source $KOBMAN_DIR/bin/kobman-init.sh
+    else
+        status="false"
+        echo "kob not found"
+        echo "Please install KOBman first and try again"
+        echo "Exiting!!!"
+        exit 1
+
+    fi
+
+    __kobman_echo_no_colour "Checking for installed version file...."
+    env_folder=($(find $KOBMAN_DIR/var/ -name "version.txt" -print))
+    env_version=($(find $KOBMAN_DIR/var/ -name "version.txt" -print))
+    if [[ -z $env_version ]]; then
+         __kobman_echo_no_colour "No version file found"
+         __kobman_echo_no_colour "Please re-install and try again"
+         __kobman_echo_no_colour "Exiting!!!"
+         status="false"
+         return 1
+    else
+         __kobman_echo_no_colour "version file found "
+         __kobman_echo_no_colour "Proceeding with the test..."
+    fi
+
+}
+
+function __test_kob_execute
+{
+    __kobman_echo_no_colour "Executing status command"
+    kob status >> tmp.txt
+    #kob version >> tmp1.txt
+
+}
+
+function __test_kob_validate
+{
+
+    __kobman_echo_no_colour "Validating...."
+    for i in "${env_folder[@]}"; do
+        n=${i##*_}
+        cat tmp.txt | grep -qw $n
+        if [[ "$?" != "0" ]]; then
+            __kobman_echo_no_colour "could not find environemnt $n in the status"
+            status="false"
+            return 1
+        fi
+
+        cat tmp.txt | grep -w $n | grep -w "$(cat $i/current)" | grep -q "*"
+        if [[ "$?" != "0" ]]; then
+            __kobman_echo_no_colour "The current version is not represented properly for $n"
+            status=false
+            return 1
+        fi
+    done
+
+    cat tmp.txt | grep -w $(cat $KOBMAN_DIR/var/current) | grep -q "~"
+    if [[ "$?" != "0" ]]; then
+        __kobman_echo_no_colour "~ is not against the last installed environment"
+        status="false"
+        return 1
+    fi
+}
+
+function __test_kob_cleanup
+{
+    rm tmp.txt
+}
+
+function __test_kob_run
+{
+    if [[ $status == "true" ]]; then
+        __test_kob_init
+    else
+        return 1
+    fi
+
+    if [[ $status == "true" ]]; then
+        __test_kob_execute
+    else
+        return 1
+    fi
+
+    if [[ $status == "true" ]]; then
+        __test_kob_validate
+    else
+        __test_kob_cleanup
+        return 1
+    fi
+    __test_kob_cleanup
+
+}
+
+__test_kob_run
+if [[ $status == "true" ]]; then
+    __kobman_echo_green "test-kob-version success"
+else
+    __kobman_echo_red "test-kob-version failed"
+fi
