7c63ffcdc31c46a27e3adf66afdb15cebe5b1fb5
EtricKombat <ericjoy1991@gmail.com>
2020-08-07 17:27:52 +0530

    branch namne chnaged

--- bin/release.sh
+++ bin/release.sh
@@ -24,7 +24,7 @@ fi
 
 # prepare branch
 cd ~/KOBman
-git checkout master
+git checkout user_config_test
 git branch -D $branch
 git checkout -b $branch
 
@@ -52,5 +52,5 @@ git push -f -u origin $branch
 git tag -a $kob_version -m "Releasing version $kob_version"
 git push origin $kob_version
 
-#checkout to master
-git checkout master
+#checkout to user_config_test
+git checkout user_config_test
